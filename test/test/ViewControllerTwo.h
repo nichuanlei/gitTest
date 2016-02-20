5671caf4047181948a6c2f754e0437b702277cc2
nichuanlei <nichuanlei@qq.com>
2016-02-20 11:50:16 +0800

    增加了ViewControllerTwo 这个类

---
+++ test/test/ViewControllerTwo.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerTwo.h
+//  test
+//
+//  Created by 倪传磊 on 16/2/20.
+//  Copyright © 2016年 nichuanlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerTwo : UIViewController
+
+@end
---
+++ test/test/ViewControllerTwo.m
@@ -0,0 +1,37 @@
+//
+//  ViewControllerTwo.m
+//  test
+//
+//  Created by 倪传磊 on 16/2/20.
+//  Copyright © 2016年 nichuanlei. All rights reserved.
+//
+
+#import "ViewControllerTwo.h"
+
+@interface ViewControllerTwo ()
+
+@end
+
+@implementation ViewControllerTwo
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
